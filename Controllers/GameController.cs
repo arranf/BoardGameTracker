50deae2b691b4459d0e67ecc2c961132a022c805
Arran France <arranf@gmail.com>
2017-06-19 21:33:23 +0100

    Add Game Controller

---
+++ Controllers/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BoardGameTracker.Data;
+using BoardGameTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoardGameTracker.Controllers
+{
+    [Route("api/[controller]")]
+    public class GameController : Controller
+    {
+        private readonly BoardGameContext _context;
+
+        public GameController (BoardGameContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Game> GetGames()
+        {
+           return _context.Games.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetGame(long id)
+        {
+            var item = _context.Games.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+    }
+}
